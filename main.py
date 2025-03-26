74c5f00f3ad1bbb808217773a207d86d3b781ed6
Rafał Rusin <rafal.rusin@o365.us.edu.pl>
2025-03-26 19:01:07 +0100

    M7L2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from model import detect_class
+from os import remove
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,10 +20,25 @@ async def hello(ctx):
 async def receive(ctx):
     if len(ctx.message.attachments) > 0:
         for attachment in ctx.message.attachments:
-            await attachment.save(f"./files/{attachment.filename}")
-            await ctx.send("Plik zapisany")
+            names = {
+                "Pigeon": "Gołębia",
+                "Sparrow": "Wróbla"
+            }
+
+            file_path = f"./files/{attachment.filename}"
+            await attachment.save(file_path)
+            await ctx.send("Przetwarzanie pliku, proszę czekać...")
+
+            result = detect_class(file_path)
+
+            if result[1] > 0.8:
+                await ctx.send(f"Udało się rozpoznać {names[result[0]]}")
+            else:
+                await ctx.send("Niestety nie udało się rozpoznać co to jest za ptak")
+            
+            remove(file_path)
     else:
         await ctx.send("Ta komenda oczekuje załączenia do niech co najmniej jednego obrazka!")
 
 
-bot.run("TOKEN")
+bot.run("")
